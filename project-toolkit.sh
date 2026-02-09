857002c9d4f371fcbccd4521bd879b29f8223b2e
tiryaki <tiryakiemre18@gmail.com>
2026-02-09 14:48:49 +0300

    Enhance project removal command to check for fzf existence before prompting for project selection.

--- project-toolkit.sh
+++ project-toolkit.sh
@@ -311,8 +311,17 @@ _cmd_rm() {
     done
 
     if [[ -z "$project_name" ]]; then
-        _get_msg "err_name_missing" "" "rm"
-        return 1
+        if ! _check_program_existence "fzf"; then
+            _get_msg "err_name_missing" "" "rm"
+            return 1
+        fi
+
+        project_name=$(find "$PROJECT_WORKSPACE" -maxdepth 1 -type d -iname "*$search_term*" -printf "%f\n" | fzf --height=40% --layout=reverse --border --prompt="$(_get_msg "info_select_project")")
+
+        if [[ -z "$project_name" ]]; then
+            _get_msg "err_name_missing" "" "rm"
+            return 1
+        fi
     fi
 
     local target="$PROJECT_WORKSPACE/$project_name"
